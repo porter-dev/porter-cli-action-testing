8e80fc799d05b38eec6ac1d4d118059060ac9153
jose-fully-ported <141160579+jose-fully-ported@users.noreply.github.com>
2023-10-26 17:34:22 -0400

    feat: add debugging info

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,12 @@
 
 : "${INPUT_COMMAND:?input \"command\" not set or empty}"
 
+echo "====> Start Docker Info"
+docker info
+echo "====> End Docker Info"
+
+echo "====> Start env"
+env
+echo "====> End env"
+
 /bin/sh -c "porter $INPUT_COMMAND"
