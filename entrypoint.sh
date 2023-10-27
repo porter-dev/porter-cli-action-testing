46055f2cdabb7dff573df85bece4c781e2ffeb47
jose-fully-ported <141160579+jose-fully-ported@users.noreply.github.com>
2023-10-27 10:31:04 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,4 @@
 
 : "${INPUT_COMMAND:?input \"command\" not set or empty}"
 
-echo "====> Start Docker Info"
-docker info
-echo "====> End Docker Info"
-
-echo "====> Start buildx ls"
-docker buildx ls
-echo "====> End buildx ls"
-
-echo "====> Start env"
-env
-echo "====> End env"
-
-set -x
-echo "====> Start build"
-docker build -f Dockerfile --cache-to type=gha --cache-from type=gha . || true
-echo "====> End build"
-set +x
-
-sleep 3
-
-
 /bin/sh -c "porter $INPUT_COMMAND"
